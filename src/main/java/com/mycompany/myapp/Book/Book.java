2aab347c1fd9cd5093daa6fe7ab551ddeddb377c
user <glgltjdwns@naver.com>
2022-02-21 15:07:45 +0900

    Commit

---
+++ src/main/java/com/mycompany/myapp/Book/Book.java
@@ -0,0 +1,20 @@
+package com.mycompany.myapp.Book;
+
+public class Book {
+
+	String seq= "";
+	String name = "";
+	Integer price = 0;
+	String author ="";
+	String publisher ="";
+	
+//	도서조회
+	
+//	도서등록
+
+//	도서수정
+
+//	도서삭제
+	
+	
+}
---
+++ src/main/java/com/mycompany/myapp/Book/BookController.java
@@ -0,0 +1,55 @@
+package com.mycompany.myapp.Book;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class BookController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+//	도서조회 : bookList
+	
+//	도서등록 : bookForm
+
+//	도서수정 : bookForm
+	
+//	도서수정 : bookView
+
+//	도서삭제 프로세스 : bookDele
+	
+//  도서수정 프로세스 : bookupde	
+	
+	
+//	도서수정 프로세스 : bookUpde
+
+	
+}
